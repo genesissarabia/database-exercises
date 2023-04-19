618ee038000e152edc9677b5f72d819bfa41ee93
Genesis Sarabia <genesissarabia14@gmail.com>
2023-04-19 13:55:05 -0500

    completed albums_seeder part 1

---
+++ albums_seeder.sql
@@ -0,0 +1,3 @@
+USE codeup_test_db;
+    INSERT INTO albums(artist, name, release_date, sales, genre)
+    VALUES('Led Zeppelin', 'Led Zeppelin IV', 1971, 30.4, 'Hard Rock'), ('Michael Jackson', 'Bad', 1987, 21.2, 'Pop'), ('Alanis Morissette', 'Jagged Little Pill', 1995, 25.4, 'Alternative Rock'), ('Bill Medley', 'Dirty Dancing', 1987, 24.1, 'Pop'), ('Celine Dion', 'Falling Into You', 1996, 21.1, 'Pop');
